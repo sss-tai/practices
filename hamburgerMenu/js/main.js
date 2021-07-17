4e8a5f53009a9dc7a279aa0c2dd1f335f059d215
sss-tai <taitai.55.taisei@gmail.com>
2021-07-17 09:31:29 +0900

    Menu表示アニメーション

--- hamburgerMenu/js/main.js
+++ hamburgerMenu/js/main.js
@@ -5,9 +5,16 @@
   const open = document.getElementById('open');
   // id振ってない。classのみ振っている。
   const overlay = document.querySelector('.overlay');
+  // id振っている
+  const close = document.getElementById('close');
 
   open.addEventListener('click', () => {
     overlay.classList.add('show');
     open.classList.add('hide');
+  });
+
+  close.addEventListener('click', () => {
+    overlay.classList.remove('show');
+    open.classList.remove('hide');
   });
 }
